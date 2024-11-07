95d697acfca5364c011c5e346bfe90a8348c89fc
H2Vid <dstmpl17@gmail.com>
2024-11-07 17:44:36 +0700

    menambahkan break-word

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import About from "@/components/about/about"
 import HeroSection from "@/components/heroSection/heroSection"
 import { Navbar } from "@/components/navbar/Navbar"
+import OurService from "@/components/ourService/service"
 
 export default function Home() {
   return (
     <div>
-      {/* <Navbar /> */}
-      {/* <HeroSection /> */}
+      <Navbar />
+      <HeroSection />
       <About />
+      <OurService />
     </div>
   )
 }
+
--- src/components/heroSection/heroSection.jsx
+++ src/components/heroSection/heroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
         <div className="grid max-w-screen-2xl px-[3%] py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 z-10">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-8 text-4xl font-extrabold tracking-wide leading-none md:text-3xl xl:text-4xl dark:text-white">Perawatan terbaik dimulai dengan perhatian terhadap diri sendiri.</h1>
-            <p className="max-w-2xl mb-6 font-light text-[#757575] lg:mb-8 md:text-lg lg:text-xl dark:text-white">
+            <p className="max-w-2xl mb-6 font-light text-[#757575]  break-words lg:mb-8 md:text-lg lg:text-xl dark:text-white">
               Mengutamakan kesehatan adalah bentuk cinta kepada diri sendiri. Jika kita bisa merawat orang lain, kita juga perlu memberi perhatian penuh pada tubuh dan pikiran kita.
             </p>
             <a
