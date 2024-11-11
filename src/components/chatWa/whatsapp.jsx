a4fb412edf4cbce0204b11c2c8d2bafde73862e5
H2Vid <dstmpl17@gmail.com>
2024-11-11 20:23:10 +0700

    Menambahkan Button redirect ke wa

--- src/components/chatWa/whatsapp.jsx
+++ src/components/chatWa/whatsapp.jsx
@@ -1,36 +1,56 @@
+"use client"
 import React from "react"
 import Image from "next/image"
+import * as Io from "react-icons/io"
 
 const Whatsapp = () => {
+  // Nomor WhatsApp klinik (gunakan nomor Anda di sini)
+  const waNumber = "+6282276492526" // ganti dengan nomor WhatsApp klinik Anda
+
+  // Fungsi untuk menangani klik tombol Kirim Pesan
+  const handleClick = () => {
+    // URL WhatsApp API
+    const url = `https://wa.me/${waNumber}`
+
+    // Membuka WhatsApp
+    window.open(url, "_blank")
+  }
   return (
-    <div className="flex items-center bg-gray-500 p-8 rounded-lg shadow-lg">
+    <div className="flex flex-col lg:flex-row lg:justify-between items-center mx-auto w-[95%] lg:w-[85%] rounded-lg mt-10">
       {/* Bagian Gambar */}
-      <div className="relative w-1/2 bg-red-400">
+      <div className="relative w-full lg:w-[400px] mb-6 lg:mb-0">
         <Image
           src="/img/Paru.png" // Gantilah dengan URL atau path gambar yang benar
           alt="Consultation Image"
           width={1000}
           height={1000}
-          className="rounded-lg w-full"
+          className="rounded-lg w-full h-auto"
         />
         {/* Thumbnail kecil */}
-        <div className="z-10 absolute bottom-4 right-0 border-2 border-white rounded-lg overflow-hidden w-32 h-32">
+        <div className="z-10 absolute bottom-0 right-0 p-2 rounded-lg overflow-hidden w-32 h-32">
           <Image
-            src="/img/Paru.png" // Gantilah dengan URL atau path gambar yang sama untuk thumbnail
+            src="/img/paru2.png" // Gantilah dengan URL atau path gambar yang sama untuk thumbnail
             alt="Thumbnail"
             width={1000}
             height={1000}
-            className="object-cover"
+            className="object-cover w-full h-full"
           />
         </div>
       </div>
+
       {/* Bagian Teks */}
-      <div className="w-1/2 ml-8">
-        <h2 className="text-4xl font-bold text-gray-800">Konsultasi & Telekonsultasi Dokter</h2>
-        <p className="mt-4 text-gray-600">Prodia Women menyediakan layanan konsultasi dan telekonsultasi dokter untuk kebutuhan kesehatan wanita.</p>
-        <button className="mt-6 px-6 py-2 bg-yellow-400 text-gray-800 font-semibold rounded-lg hover:bg-yellow-500 flex items-center">
-          Selengkapnya
-          <span className="ml-2">→</span>
+      <div className="w-full lg:w-1/2 ml-0 space-y-8 lg:ml-8">
+        <h2 className="text-3xl lg:text-4xl font-bold text-gray-800">Konsultasi & Telekonsultasi Dokter</h2>
+        <p className="mt-4 text-gray-600 text-lg">
+          Jangan biarkan masalah kesehatan mengganggu hidup Anda. Dengan layanan konsultasi yang mudah dan cepat, Anda bisa langsung mendapatkan saran medis dari dokter spesialis. Segera buat janji dan dapatkan solusi terbaik untuk
+          kesehatan Anda!
+        </p>
+        <button
+          onClick={handleClick}
+          className="items-center flex space-x-3 before:ease relative h-20 w-72 justify-center overflow-hidden rounded-lg border border-[#00ADAA] bg-[#00ADAA] text-white transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-green-500 hover:before:-translate-x-40"
+        >
+          <Io.IoLogoWhatsapp size={32} />
+          <span className="relative z-10 text-lg font-semibold">Chat Sekarang Juga</span>
         </button>
       </div>
     </div>
