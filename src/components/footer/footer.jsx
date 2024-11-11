6405f8bb3fc4741eaa4f60eac41f3326d24a5f6d
H2Vid <dstmpl17@gmail.com>
2024-11-12 00:20:12 +0700

    change tag <a> to next Link <Link>

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import React from "react"
 
 const Footer = () => {
@@ -7,7 +8,7 @@ const Footer = () => {
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="/" className="flex items-center">
+            <Link href="/" className="flex items-center">
               <Image
                 width={1000}
                 height={1000}
@@ -16,7 +17,7 @@ const Footer = () => {
                 alt="Logo Klinik Sehat"
               />
               <span className="self-center text-2xl font-semibold text-gray-100 dark:text-white">Klinik Sehat</span>
-            </a>
+            </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             {/* Layanan Kami */}
@@ -24,24 +25,24 @@ const Footer = () => {
               <h2 className="mb-6 text-sm font-semibold text-gray-100 uppercase dark:text-white">Layanan Kami</h2>
               <ul className="text-gray-100 dark:text-gray-100 font-medium">
                 <li className="mb-4">
-                  <a href="/dokter" className="hover:underline">
+                  <Link href="/dokter" className="hover:underline">
                     Daftar Dokter
-                  </a>
+                  </Link>
                 </li>
                 <li className="mb-4">
-                  <a href="/pemeriksaan" className="hover:underline">
+                  <Link href="/pemeriksaan" className="hover:underline">
                     Layanan Pemeriksaan
-                  </a>
+                  </Link>
                 </li>
                 <li className="mb-4">
-                  <a href="/jadwal" className="hover:underline">
+                  <Link href="/jadwal" className="hover:underline">
                     Jadwal Klinik
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="/contact" className="hover:underline">
+                  <Link href="/contact" className="hover:underline">
                     Kontak & Lokasi
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -51,22 +52,22 @@ const Footer = () => {
               <h2 className="mb-6 text-sm font-semibold text-gray-100 uppercase dark:text-white">Kontak Kami</h2>
               <ul className="text-gray-100 dark:text-gray-100 font-medium">
                 <li className="mb-4">
-                  <a href="tel:+62123456789" className="hover:underline">
+                  <Link href="tel:+62123456789" className="hover:underline">
                     Telp: +62 123 456 789
-                  </a>
+                  </Link>
                 </li>
                 <li className="mb-4">
-                  <a href="mailto:info@kliniksehat.com" className="hover:underline">
+                  <Link href="mailto:info@kliniksehat.com" className="hover:underline">
                     Email: info@kliniksehat.com
-                  </a>
+                  </Link>
                 </li>
                 <li className="mb-4">
-                  <a
+                  <Link
                     href="https://goo.gl/maps/xyz123" // Ganti dengan Google Maps link untuk lokasi klinikmu
                     className="hover:underline"
                   >
                     Alamat: Jl. Sehat No. 123, Kota Sehat
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -76,19 +77,19 @@ const Footer = () => {
               <h2 className="mb-6 text-sm font-semibold text-gray-100 uppercase dark:text-white">Ikuti Kami</h2>
               <ul className="text-gray-100 dark:text-gray-100 font-medium">
                 <li className="mb-4">
-                  <a href="https://facebook.com/kliniksehat" className="hover:underline">
+                  <Link href="https://facebook.com/kliniksehat" className="hover:underline">
                     Facebook
-                  </a>
+                  </Link>
                 </li>
                 <li className="mb-4">
-                  <a href="https://instagram.com/kliniksehat" className="hover:underline">
+                  <Link href="https://instagram.com/kliniksehat" className="hover:underline">
                     Instagram
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="https://twitter.com/kliniksehat" className="hover:underline">
+                  <Link href="https://twitter.com/kliniksehat" className="hover:underline">
                     Twitter
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -100,26 +101,26 @@ const Footer = () => {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-100 sm:text-center dark:text-gray-100">
             © 2023{" "}
-            <a href="/" className="hover:underline">
+            <Link href="/" className="hover:underline">
               Klinik Sehat™
-            </a>
+            </Link>
             . Semua Hak Dilindungi
           </span>
 
           <div className="flex mt-4 sm:justify-center sm:mt-0">
-            <a href="https://facebook.com/kliniksehat" className="text-gray-100 hover:text-gray-100 dark:hover:text-white">
+            <Link href="https://facebook.com/kliniksehat" className="text-gray-100 hover:text-gray-100 dark:hover:text-white">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                 <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
               </svg>
               <span className="sr-only">Facebook page</span>
-            </a>
-            <a href="https://instagram.com/kliniksehat" className="text-gray-100 hover:text-gray-100 dark:hover:text-white ms-5">
+            </Link>
+            <Link href="https://instagram.com/kliniksehat" className="text-gray-100 hover:text-gray-100 dark:hover:text-white ms-5">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
                 <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
               </svg>
               <span className="sr-only">Instagram page</span>
-            </a>
-            <a href="https://twitter.com/kliniksehat" className="text-gray-100 hover:text-gray-100 dark:hover:text-white ms-5">
+            </Link>
+            <Link href="https://twitter.com/kliniksehat" className="text-gray-100 hover:text-gray-100 dark:hover:text-white ms-5">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
                 <path
                   fillRule="evenodd"
@@ -128,7 +129,7 @@ const Footer = () => {
                 />
               </svg>
               <span className="sr-only">Twitter page</span>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
